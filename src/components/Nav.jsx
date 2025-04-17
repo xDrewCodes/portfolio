7f27e80d78566f931d2a48aa4c49f5c9031fa1f9
Drew Allred <drew.e.allred@gmail.com>
2025-04-17 10:32:24 -0400

    landing redesign

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,9 +5,12 @@ const Nav = () => {
 
     return (
         <nav className="row">
-            <div className="nav__item">My Projects</div>
-            <div className="nav__item">Contact Me</div>
-            <div className="nav__item">My Experience</div>
+            <div className="nav__logo">DA</div>
+            <div>
+                <div className="nav__item">My Projects</div>
+                <div className="nav__item">Contact Me</div>
+                <div className="nav__item">My Experience</div>
+            </div>
         </nav>
     )
 }
